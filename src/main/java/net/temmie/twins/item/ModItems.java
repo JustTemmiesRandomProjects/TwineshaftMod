26fef764d9244fcb13f2f1a4826a9b3ac1b2c80c
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-01-12 22:05:11 +0100

    Initial commit

---
+++ src/main/java/net/temmie/twins/item/ModItems.java
@@ -0,0 +1,24 @@
+package net.temmie.twins.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.temmie.twins.TwinsAPI;
+
+public class ModItems {
+
+    public static final Item TELEPORT_STAR = registerItem("teleport_star", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item FIREBALL = registerItem("fireball_spell", new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(TwinsAPI.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        System.out.println("Registering Mod Items for " + TwinsAPI.MOD_ID);
+    }
+}
---
+++ src/main/java/net/temmie/twins/mixin/TwinsMixin.java
@@ -0,0 +1,16 @@
+package net.temmie.twins.mixin;
+
+import net.temmie.twins.TwinsAPI;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TwinsMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		TwinsAPI.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}

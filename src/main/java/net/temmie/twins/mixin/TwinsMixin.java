855daca84fdafdce485859d882c5839a1d276c3e
Temmie <47639983+JustTemmie@users.noreply.github.com>
2022-04-13 19:56:35 +0200

    Update TwinsMixin.java

--- src/main/java/net/temmie/twins/mixin/TwinsMixin.java
+++ src/main/java/net/temmie/twins/mixin/TwinsMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TwinsMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		TwinsAPI.LOGGER.info("This line is printed by an example mod mixin!");
+		TwinsAPI.LOGGER.info("Beaver!");
 	}
 }
